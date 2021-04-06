057bb9cbc7c68ae0a30f660971e0babb190858ee
Vladyslav <vladyslav@MacBook-Pro.local>
2021-04-06 19:57:30 +0300

    first commit

---
+++ DoubleLearning.java
@@ -0,0 +1,21 @@
+package PracticeJavaBasics;
+
+public class DoubleLearning {
+
+    public static void main(String args[]) {
+        int var;
+        double x;
+
+        var = 10;
+        x = 10.0;
+
+        System.out.println("Initial value of var: " + var);
+        System.out.println("Initial value of x: " + x);
+
+        var = var / 4;
+        x = x / 4;
+
+        System.out.println("var divided by 4: " + var);
+        System.out.println("x divided by 4: " + x);
+    }
+}
